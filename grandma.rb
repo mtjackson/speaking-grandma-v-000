f0f2db091ae0e97da0c898664c0b7f40e7852f30
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-23 02:06:12 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -6,8 +6,7 @@ def speak_to_grandma(grandma_string)
     puts "HUH?! SPEAK UP, SONNY!"
   elsif grandma_string == "WHAT DID YOU EAT TODAY?"
     puts "NO, NOT SINCE 1938!"
-
-
+  end
 end
 
 # Whatever you say to grandma, she should respond with
